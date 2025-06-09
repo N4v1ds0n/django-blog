660df782cb72364b465f156002d8e7885cf939eb
N4v1ds0n <damian.droste@gmx.de>
2025-06-09 23:13:33 +0200

    Adjust settings to heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z_%&c(w3=g*kr7rqfmdm$!_u=mlkd9_g+scdfsaadh@8-kry=z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'herokuapp.com', '.herokuapp.com']
 
 
 # Application definition
