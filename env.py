18da37e27fe57066e703dcfd23dbda622d9523fc
N4v1ds0n <damian.droste@gmx.de>
2025-06-10 14:35:51 +0200

    Remove env.py from github and change db

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_cf9nOg1kdNUC@ep-tiny-bird-a2d7v6so.eu-central-1.aws.neon.tech/sport_sweat_poet_577186")
+    "DATABASE_URL", "postgresql://neondb_owner:npg_cf9nOg1kdNUC@ep-tiny-bird-a2d7v6so.eu-central-1.aws.neon.tech/snap_cargo_erase_121977")
