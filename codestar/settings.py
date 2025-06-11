50e172ce7a3b2a80f6af4c9df65c437406d3ddaa
N4v1ds0n <damian.droste@gmx.de>
2025-06-11 10:20:15 +0200

    Handle flake8 exceptions

--- codestar/settings.py
+++ codestar/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
-    import env
+    import env                                     # noqa
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')  # 'django-insecure-z_%&c(w3=g*kr7rqfmdm$!_u=mlkd9_g+scdfsaadh@8-kry=z'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
